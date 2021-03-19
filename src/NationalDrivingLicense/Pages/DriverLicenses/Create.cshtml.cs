a2e22b101a4127224ceb8b6cc2f1ea0a8596508f
damienbod <damien_bod@hotmail.com>
2021-03-19 15:54:54 +0100

    Adding driver license CRUD

---
+++ src/NationalDrivingLicense/Pages/DriverLicenses/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NationalDrivingLicense.Data;
+
+namespace NationalDrivingLicense.Pages.DriverLicenses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly NationalDrivingLicense.Data.ApplicationDbContext _context;
+
+        public CreateModel(NationalDrivingLicense.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DriverLicence DriverLicence { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            DriverLicence.Issuedby = HttpContext.User.Identity.Name;
+            DriverLicence.IssuedAt = DateTimeOffset.UtcNow;
+
+            _context.DriverLicences.Add(DriverLicence);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/NationalDrivingLicense/Pages/DriverLicenses/UpdateDriverLicence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NationalDrivingLicense.Pages.DriverLicenses
+{
+    public class UpdateDriverLicence
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public bool Valid { get; set; }
+    }
+}
