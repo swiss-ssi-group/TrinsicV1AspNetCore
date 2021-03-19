9f258d0230f8befa72064290d77b56b198b4f3e1
damienbod <damien_bod@hotmail.com>
2021-03-19 17:41:42 +0100

    Adding MFA app setting

--- src/NationalDrivingLicense/Pages/Admin.cshtml.cs
+++ src/NationalDrivingLicense/Pages/Admin.cshtml.cs
@@ -4,23 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace NationalDrivingLicense
 {
     public class AdminModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public AdminModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IActionResult OnGet()
         {
             var claimTwoFactorEnabled = User.Claims.FirstOrDefault(t => t.Type == "amr");
 
             if (claimTwoFactorEnabled != null && "mfa".Equals(claimTwoFactorEnabled.Value))
             {
-                // You logged in with MFA, do the admin stuff
+                // You logged in with MFA, or MFA is disabled, do the admin stuff
             }
             else
             {
-                // to test mfa check disabled
-                // return Redirect("/Identity/Account/Manage/TwoFactorAuthentication");
+                var requireMfa = bool.Parse(_configuration["MfaRequiredForAdmin"]);
+                if(requireMfa)
+                {
+                    return Redirect("/Identity/Account/Manage/TwoFactorAuthentication");
+                }
             }
 
             return Page();
